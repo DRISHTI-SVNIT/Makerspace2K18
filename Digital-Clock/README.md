ce7ceac04d3822790b853ee3f7e1af871f87f6d2
Dhruv Patel <30570613+Dhruv2012@users.noreply.github.com>
2019-01-20 01:18:31 +0530

    Revert "Digital-Clock"

--- Digital-Clock/README.md
+++
@@ -1,6 +0,0 @@
-# Real_Time_Clock
-Atmel File 
-
-IC:DS1307
-
-Libraries: USART and I2C
--- Digital-Clock/Real_time_clock/usart.h
+++
@@ -1,9 +0,0 @@
-#ifndef usart
-#define usart
-
-
-void u_initiate();
-void u_transmit(unsigned char data);
-unsigned char  u_recieve();
-
-#endif
